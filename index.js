90c9bb836aaff64d554beab3cb80e6ed194b2cbe
sukhvinder13 <sukhvindersingh2513@gmail.com>
2025-07-27 21:31:47 +0530

    node port

--- index.js
+++ index.js
@@ -9,3 +9,8 @@ mongoose.connect(process.env.MONGO_URI, {
   console.log('MongoDB connected');
   require('./bot/bot'); // Start bot
 }).catch(err => console.error(err));
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Telegram bot is running!'));
+app.listen(5000, () => console.log('HTTP endpoint live on port 3000'));
